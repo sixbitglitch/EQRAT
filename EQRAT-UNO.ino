96a382a70e23fbcebf55c09011df4c438d77a6c0
16BitGlitch <16bitglitch@gmail.com>
2022-01-16 13:22:53 +0000

    Auto Calc + Readme corrections
    1) Added auto calculation of timer from mount/motor parameters
    2) Renamed sketch to match repo.
    3) Updated the description to correct missing bits in calcs

--- EQRAT-UNO.ino
+++ EQRAT-UNO.ino
@@ -1,3 +1,4 @@
+/*  
 # EQRAT-UNO
 Simple Equatorial Mount Ra Sidereal Tracker for Arduino UNO/328
 
@@ -58,3 +59,110 @@ Step_Delay_Microseconds = (239.34/6933.333333)  1000000 = 34520.8
 Step_Delay_Timer_Half_Phase = 34520.8 / 2 = 17260.4
    
 OCR1A= ( 16000000 / (1/(17260.4))*1000000)*8) ) -1 = 34520 (rounded to an int)
+ 
+
+*/
+ 
+//Debug Stuff
+const bool debugEnabled=true;
+
+//Configuration
+const int Mount_Worm_Gear_Ratio=130;
+const int Motor_Gear_Ratio=3;
+const int Steps_Per_Rev=200;
+const int Microstep_Setting=32;
+
+//Stuff for timer calc (doing everything as floats until its time to convert to timer)
+
+const float Seconds_Earth_Rotate=86164.09053;
+const float processorSpeed=16000000;
+
+
+float Earth_Seconds_Per_Degree =Seconds_Earth_Rotate / 360.0;
+float MicroSteps_Per_Degree =((float)Mount_Worm_Gear_Ratio * (float)Motor_Gear_Ratio *  (float)Steps_Per_Rev * (float)Microstep_Setting) / 360.0;
+float Step_Delay_Microseconds =(Earth_Seconds_Per_Degree / MicroSteps_Per_Degree) * 1000000.0;
+float Step_Delay_Timer_Half_Phase=Step_Delay_Microseconds / 2.0;
+
+float OCR1A_Calc_Value = ( 16000000.0 / (((1.0/(Step_Delay_Timer_Half_Phase)) * 1000000.0) * 8.0)) -1.0;
+unsigned int OCR1A_Value = round(OCR1A_Calc_Value);
+
+long lastTime=0;
+long currentTime=0;
+
+
+//Ra Stepper Config
+const int dirPin = 5;   
+const int stepPin = 2;    
+
+//Ra Stepper State
+uint8_t raStepState=LOW;
+
+// Generated with http://www.arduinoslovakia.eu/application/timer-calculator
+void setupTimer1() {
+  noInterrupts();
+  
+  // Clear registers
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+
+  //EDIT THIS TO SET TIMER CORRECTLY  - See above
+  OCR1A=OCR1A_Value;
+  //OCR1A = 34519;
+  //EDIT THIS TO SET TIMER CORRECTLY - See above
+
+  // CTC
+  TCCR1B |= (1 << WGM12);
+  // Prescaler 8
+  TCCR1B |= (1 << CS11);
+  // Output Compare Match A Interrupt Enable
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
+}
+
+void setup() {  
+  
+  Serial.begin(9600);
+  Serial.println("Starting EQRAT");
+  Serial.println("--------------");
+  Serial.println("Timer Calc : " + String(OCR1A_Value));
+  delay(500000);
+
+  //Setup Pins  
+  pinMode(stepPin, OUTPUT);   
+  pinMode(dirPin, OUTPUT);    
+  digitalWrite(dirPin, LOW);   // invert this (HIGH) if wrong direction    
+
+  //Setup and start Timer
+  setupTimer1();
+    
+}   
+
+ISR(TIMER1_COMPA_vect) {
+  
+  //Switch the step pin state
+  if(raStepState==LOW){
+    raStepState=HIGH;
+  }
+  else{
+    raStepState=LOW;
+  }
+
+   //Write to the step pin
+   digitalWrite(stepPin, raStepState); 
+
+   //Do some counting if debug is enabled
+   if(debugEnabled)
+      timerCount();
+}
+
+void timerCount(){
+  lastTime=currentTime;
+  currentTime=micros();
+}
+
+void loop() {  
+  //Spit out some debug stuff
+  if(debugEnabled)
+    Serial.println(String((currentTime-lastTime)*2));
+}   
