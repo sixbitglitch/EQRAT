862e91dd81d15b6cd178f152cfa37380b9326fc8
sixbitglitch <charles@realityboffins.com>
2025-09-07 13:55:08 +0100

    Updated

--- EQRAT.ino
+++ EQRAT.ino
@@ -1,114 +1,108 @@
 /*  
-# EQRAT-UNO
-##Simple Equatorial Mount Ra Sidereal Tracker for Arduino UNO/328
+EQRAT-UNO
+Simple Equatorial Mount RA Sidereal Tracker for Arduino UNO/328
 
 By Charles Gershom 
 @charlesgershom 
 
-See Readme.md
-
 */
 
-//Debug Stuff
-const bool debugEnabled=true;
+//Debug
+const bool debugEnabled = true;
 
-//Configuration
-const int Mount_Worm_Gear_Ratio_RA=130; //RA SKYWATCHER EQ 3-2
-const int Mount_Worm_Gear_Ratio_Dec=65; //DEC SKYWATCHER EQ 3-2
-const float Motor_Gear_Ratio_RA=2.5; //30/12 - Asteron Drive Kit Light
-const float Motor_Gear_Ratio_DEC=3; //30/12 - Asteron Drive Kit Light
-const int Steps_Per_Rev=200;
-const int Microstep_Setting=32;
+//Mount config
+const int Mount_Worm_Gear_Ratio_RA = 130; //RA SKYWATCHER EQ 3-2
+const int Mount_Worm_Gear_Ratio_Dec = 65; //DEC SKYWATCHER EQ 3-2
+const float Motor_Gear_Ratio_RA = 2.5; //30/12 - Asteron Drive Kit Light
+const float Motor_Gear_Ratio_DEC = 3.0; //30/12 - Asteron Drive Kit Light
+const int Steps_Per_Rev = 200;
+const int Microstep_Setting = 32;
 
-//Stuff for timer calc (doing everything as floats until its time to convert to timer)
-
-const float Seconds_Earth_Rotate=86164.09053;
-const float processorSpeed=16000000;
-
-float Earth_Seconds_Per_Degree =Seconds_Earth_Rotate / 360.0;
-float MicroSteps_Per_Degree =((float)Mount_Worm_Gear_Ratio_RA * (float)Motor_Gear_Ratio_RA *  (float)Steps_Per_Rev * (float)Microstep_Setting) / 360.0;
-float Step_Delay_Microseconds =(Earth_Seconds_Per_Degree / MicroSteps_Per_Degree) * 1000000.0;
-float Step_Delay_Timer_Half_Phase=Step_Delay_Microseconds / 2.0;
-
-float OCR1A_Calc_Value = ( 16000000.0 / (((1.0/(Step_Delay_Timer_Half_Phase)) * 1000000.0) * 8.0)) -1.0;
-unsigned int OCR1A_Value = round(OCR1A_Calc_Value);
-
-long lastTime=0;
-long currentTime=0;
-
-//Ra Stepper Config
+//Pin assignments
 const int RAdirPin = 9;   
 const int RAstepPin = 10;    
-
-//Dec Stepper Config
 const int DECdirPin = 4;   
 const int DECstepPin = 3;    
-
 const int EnablePin = 12;   
 const int MicrostepSelect = 11;   
 
-//Ra Stepper State
-uint8_t raStepState=LOW;
+//Sidereal math
+const float Seconds_Earth_Rotate = 86164.09053;
+const float processorSpeed = 16000000;
 
-// Generated with http://www.arduinoslovakia.eu/application/timer-calculator
+float Earth_Seconds_Per_Degree = Seconds_Earth_Rotate / 360.0;
+float MicroSteps_Per_Degree = ((float)Mount_Worm_Gear_Ratio_RA * (float)Motor_Gear_Ratio_RA * (float)Steps_Per_Rev * (float)Microstep_Setting) / 360.0;
+float Step_Delay_Microseconds = (Earth_Seconds_Per_Degree / MicroSteps_Per_Degree) * 1000000.0;
+float Step_Delay_Timer_Half_Phase = Step_Delay_Microseconds / 2.0;
+
+float OCR1A_Calc_Value = (processorSpeed / (((1.0 / Step_Delay_Timer_Half_Phase) * 1000000.0) * 8.0)) - 1.0;
+unsigned int OCR1A_Value = round(OCR1A_Calc_Value);
+
+//Stepper state
+uint8_t raStepState = LOW;
+
+//Debug timing
+unsigned long lastTime = 0;
+unsigned long currentTime = 0;
+unsigned long stepCount = 0;
+
+//Timer setup - generated with http://www.arduinoslovakia.eu/application/timer-calculator
 void setupTimer1() {
   noInterrupts();
-  
-  // Clear registers
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1 = 0;
-  //TO SET TIMER CORRECTLY  - See above
-  OCR1A=OCR1A_Value;
-  // CTC
-  TCCR1B |= (1 << WGM12);
-  // Prescaler 8
-  TCCR1B |= (1 << CS11);
-  // Output Compare Match A Interrupt Enable
-  TIMSK1 |= (1 << OCIE1A);
+  OCR1A = OCR1A_Value;
+  TCCR1B |= (1 << WGM12); //CTC mode
+  TCCR1B |= (1 << CS11); //Prescaler 8
+  TIMSK1 |= (1 << OCIE1A); //Output Compare Match A Interrupt Enable
   interrupts();
 }
 
 void setup() {  
-  
   Serial.begin(9600);
   Serial.println("Starting EQRAT");
   Serial.println("--------------");
   Serial.println("Timer Calc : " + String(OCR1A_Value));
-  //Setup Pins  
+  
   pinMode(RAstepPin, OUTPUT);   
   pinMode(RAdirPin, OUTPUT);    
-  digitalWrite(RAdirPin, HIGH);   // invert this (HIGH) if wrong direction    
- digitalWrite(EnablePin, LOW);  
+  pinMode(DECstepPin, OUTPUT);   
+  pinMode(DECdirPin, OUTPUT);    
+  pinMode(EnablePin, OUTPUT);  
+  pinMode(MicrostepSelect, OUTPUT);  
+  
+  digitalWrite(RAdirPin, HIGH);   //invert this (HIGH) if wrong direction    
+  digitalWrite(EnablePin, LOW);  
   digitalWrite(MicrostepSelect, HIGH);  
-  //Setup and start Timer
+  
   setupTimer1();
-    
 }   
 
 ISR(TIMER1_COMPA_vect) {
-  
-  //Switch the step pin state
-  if(raStepState==LOW){
-    raStepState=HIGH;
+  if(raStepState == LOW){
+    raStepState = HIGH;
     digitalWrite(RAstepPin, raStepState); 
   }
   else{
-    raStepState=LOW;
+    raStepState = LOW;
     digitalWrite(RAstepPin, raStepState); 
   }
-   //Do some counting if debug is enabled
-   if(debugEnabled)
-      timerCount();
-}
-
-void timerCount(){
-  lastTime=currentTime;
-  currentTime=micros();
+  
+  if(debugEnabled) {
+    lastTime = currentTime;
+    currentTime = micros();
+    stepCount++;
+  }
 }
 
 void loop() {  
-  //Spit out some debug stuff
-  if(debugEnabled)
-    Serial.println(String((currentTime-lastTime)*2));
+  if(debugEnabled) {
+    static unsigned long lastPrint = 0;
+    if(millis() - lastPrint > 1000) {
+      unsigned long timeDiff = (currentTime - lastTime) * 2;
+      Serial.println("Step period: " + String(timeDiff) + "μs, Steps: " + String(stepCount));
+      lastPrint = millis();
+    }
+  }
 }   
--- README.md
+++ README.md
@@ -1,70 +1,55 @@
 # EQRAT-UNO
-##Simple Equatorial Mount Ra Sidereal Tracker for Arduino UNO/328
+Simple Equatorial Mount RA Sidereal Tracker for Arduino UNO/328
 
 By Charles Gershom 
 @charlesgershom 
 
-## About
+## Overview
 
-Originally Built to run a Skywatcher EQ 3-2 for polar aligned RA only sidereal tracking.
-This was built in response to not being able to figure out why onstep wasnt tracking right
-and because I had no real idea what the drivers were doing, what the gear stuff was, or how 
-uno timers worked. 
-   
-The intention is to have a super simple "just turn it on" sidereal motor driver for the mount since I enjoy
-finding the stars manually by eye instead of goto. This is a result of a couple nights of head scratching, info reading, and learning that delayMicroseconds on arduino overflows and goes wonky above 16383... always read the documents... 
-   
-Some of thie might be wrong or innacurate.. its working "good enough" for me, but thought it might be useful to share
+Minimal sidereal tracking implementation for equatorial mounts. Uses Timer1 CTC mode to generate precise step timing without blocking the main loop. Built for Skywatcher EQ3-2 with Asteron Drive Kit Light.
 
-*Update, recently got an Asterion Drive Kit Light, and modified this to run on that platform
+## Hardware
 
-## References
-Thanks to this thread that inspired me to make a super simple version. This is very similar, but I decided to use timers instead:
-https://www.cloudynights.com/topic/731261-yet-another-diy-ra-drive-for-eq5-exos2-eq3-etc-etc/
+- Skywatcher EQ3-2 mount
+- 0.9°/400 step NEMA 17 steppers
+- DRV8825 drivers (32 microsteps)
+- Arduino UNO
+- CNC V3 Arduino Shield
 
-## Parts Used 
-Skywatcher EQ3-2
-0.9degree/400 Step Nema 17 Motors 
-DRV8825 Drivers set to 32 Micro Steps
-Arduino UNO
-CNC V3 Arduino Shield
+## Implementation
+
+Timer1 configured for CTC mode with prescaler 8. Step timing calculated from sidereal rate and gear ratios. ISR toggles step pin state every timer overflow.
+
+Key insight: `delayMicroseconds()` overflows above 16383μs, making it unsuitable for sidereal tracking rates.
 
 ## Configuration
-   
-To modify this for your setup, input the details for your scope with the variables:
 
-Mount_Worm_Gear_Ratio
+Modify these constants for your mount:
 
-Motor_Gear_Ratio
+```cpp
+const int Mount_Worm_Gear_Ratio_RA = 130;    // RA worm gear ratio
+const float Motor_Gear_Ratio_RA = 2.5;       // Motor gear reduction
+const int Steps_Per_Rev = 200;               // Stepper steps per revolution
+const int Microstep_Setting = 32;            // Microstep setting
+```
 
-Steps_Per_Rev
+## Timing Calculations
 
-Microstep_Setting
+Sidereal rate: 86164.09053 seconds per day
 
-## Timer Calculations
-
-The sketch will auto calculate the timer value based on the information below.
-  
-Seconds_Earth_Rotate = 86164.09053
-Earth_Seconds_Per_Degree = Seconds_Earth_Rotate / 360
-MicroSteps_Per_Degree = (Mount_Worm_Gear_Ratio * Motor_Gear_Ratio *  Steps_Per_Rev * Microstep_Setting) / 360
-Step_Delay_Microseconds = (Earth_Seconds_Per_Degree / MicroSteps_Per_Degree) * 1000000
-Step_Delay_Timer_Half_Phase = Step_Delay_Microseconds / 2
-   
-I use Step_Delay_Timer_Half_Phase in the timer1 OCR1A calculations This alternates the 
-step pin Low and High to complete one full step/microstep in two phases. Timer is then calculated using:  
-   
-OCR1A= ( 16000000 / (1/(Step_Delay_Timer_Half_Phase)) * 1000000) * 8) ) -1
-   
-For reasons, see below calcs    
-https://www.unitjuggler.com/convert-frequency-from-%C2%B5s(p)-to-Hz.html?val=17256
-and 
-http://www.arduinoslovakia.eu/application/timer-calculator
-   
-Example
+```
 Earth_Seconds_Per_Degree = 86164.09053 / 360 = 239.34
-MicroSteps_Per_Degree = (130  * 3 *  200 *  32) /360 = 6933.333333
-Step_Delay_Microseconds = (239.34/6933.333333)  1000000 = 34520.8
+MicroSteps_Per_Degree = (130 × 2.5 × 200 × 32) / 360 = 6933.33
+Step_Delay_Microseconds = (239.34 / 6933.33) × 1000000 = 34520.8
 Step_Delay_Timer_Half_Phase = 34520.8 / 2 = 17260.4
-   
-OCR1A= ( 16000000 / (1/(17260.4))*1000000)*8) ) -1 = 34520 (rounded to an int)
+```
+
+Timer1 OCR1A calculation (prescaler 8):
+```
+OCR1A = (16000000 / ((1/17260.4) × 1000000) × 8) - 1 = 34520
+```
+
+## References
+
+- [Cloudy Nights DIY RA Drive Thread](https://www.cloudynights.com/topic/731261-yet-another-diy-ra-drive-for-eq5-exos2-eq3-etc-etc/)
+- [Arduino Timer Calculator](http://www.arduinoslovakia.eu/application/timer-calculator)
